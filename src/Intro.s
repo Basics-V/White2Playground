cf0988803ae819818833f621a6aaf231de71879a
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2026-07-12 02:24:09 +0200

    SKIP_INTRO_MOVIE
    Skips the intro movie and goes straight to the title screen. Hopefully not a memory leak(?) - research before implementing in your own project.

---
+++ src/Intro.s
@@ -0,0 +1,8 @@
+.thumb
+
+#ifdef SKIP_INTRO_MOVIE
+@ Move on to the title screen
+FULL_COPY_StartMenuEventTick_0x46:
+    MOV R0,#0x3
+    .size FULL_COPY_StartMenuEventTick_0x46, . - FULL_COPY_StartMenuEventTick_0x46
+#endif
