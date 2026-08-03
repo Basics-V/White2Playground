cf0b2c2d31243dbd614db46ba69c6ea426d0e7fa
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2026-08-02 20:27:53 -0500

    FISHING_NO_WAIT
    Can still get "no nibble" - rework encounter table to prevent this

---
+++ src/EventFieldFishing.s
@@ -0,0 +1,8 @@
+.thumb
+
+#ifdef FISHING_NO_WAIT
+FULL_COPY_EventFieldFishing_Callback_0xa4:
+    MOV R0,#0x2
+    NOP
+    .size FULL_COPY_EventFieldFishing_Callback_0xa4, . - FULL_COPY_EventFieldFishing_Callback_0xa4
+#endif
