277684232cc91a755f3a9fe0a0ede0e726ec6319
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2026-08-03 01:09:28 -0500

    HIDE_HM
    HIDE_DIG, HIDE_TELEPORT

---
+++ include/HIDE_HM.h
@@ -0,0 +1,4 @@
+#ifdef HIDE_HM
+#define HIDE_DIG
+#define HIDE_TELEPORT
+#endif
---
+++ src/PkmList.s
@@ -0,0 +1,19 @@
+.thumb
+
+#ifdef HIDE_HM
+#include "HIDE_HM.h"
+
+#ifdef HIDE_DIG
+@ Return 0 for move dig in list
+FULL_COPY_PkmList_MoveMatchHM_0x80:
+    MOV R0,#0x0
+    .size FULL_COPY_PkmList_MoveMatchHM_0x80, . - FULL_COPY_PkmList_MoveMatchHM_0x80
+#endif
+
+#ifdef HIDE_TELEPORT
+@ Return 0 for move teleport in list
+FULL_COPY_PkmList_MoveMatchHM_0x7c:
+    MOV R0,#0x0
+    .size FULL_COPY_PkmList_MoveMatchHM_0x7c, . - FULL_COPY_PkmList_MoveMatchHM_0x7c
+#endif
+#endif
